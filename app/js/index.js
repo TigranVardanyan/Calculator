db0806f6f2d13f703adda02acee6be92d01dcb22
Tigran <tig.vardanyan.001@gmail.com>
2020-04-16 01:00:50 +0400

    some changes

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,14 @@
 class Calculator {
   buttonSection = document.querySelector('.calc-body');
+  resultDiv = document.getElementById('result');
+  expressionDiv = document.getElementById('expression');
   result = 0;
   expression = '';
-  resultDiv = document.getElementById('result');
-  expressionDiv = document.getElementById('expression');result = 0;
   localHistory = [];
   constructor() {
     console.log('calc ready');
   }
+
   KeyPressNumber(number) {
     this.expression += number;
     this.expressionDiv.innerHTML = this.expression;
@@ -18,36 +19,65 @@ class Calculator {
   }
   KeyPressEqual() {
     this.expressionDiv.innerHTML = eval(this.expression);
+    this.expression = this.result;
   }
   KeyPressClean() {
     this.expression = '';
     this.result = 0;
+    this.CleanExpressionDiv();
+    this.CleanResultDiv();
+  }
+  KeyPressBackspace() {
+    this.expression = this.expression.slice(0,-1);
+    this.expressionDiv.innerHTML = this.expression;
+  }
+  CleanExpressionDiv() {
+    this.expressionDiv.innerHTML = "";
+  }
+  CleanResultDiv() {
+    this.resultDiv.innerHTML = "";
+  }
+
+  TransitionalResult() {
+    this.resultDiv.innerHTML = eval(this.expression);
   }
 }
 
 
-console.log(1+2+3*4);
+// console.log(1+2+3*4);
 
 let calc = new Calculator();
 let dataType;
 let targetValue;
 calc.buttonSection.addEventListener('click', (e)=> {
-  console.log(e.target);
-  if(e.target.tagName != 'BUTTON') {
-    dataType = e.target.parentElement.dataset.type;
-    targetValue = e.target.parentElement.value;
-  } else {
+  // console.log(e.target);
+
+  // if(e.target.tagName != 'BUTTON') {
+  //   dataType = e.target.parentElement.dataset.type;
+  //   targetValue = e.target.parentElement.value;
+  // } else {
     dataType = e.target.getAttribute('data-type');
     targetValue = e.target.value;
-  }
+  //}
   if(dataType == 'number') {
     calc.KeyPressNumber(targetValue);
   }
   if(dataType == 'symbol') {
+    if (calc.expression[calc.expression.length-1] != "*" && calc.expression[calc.expression.length-1] != "+" && calc.expression[calc.expression.length-1] != "-" && calc.expression[calc.expression.length-1] != "/" && calc.expression[calc.expression.length-1] != "pow"  && calc.expression[calc.expression.length-1] != "%" && calc.expression[calc.expression.length-1] != "=")
     calc.KeyPressMathSymbol(targetValue);
   }
   if(dataType == 'equal') {
     calc.KeyPressEqual();
   }
-
+  if(dataType == "clean") {
+    calc.KeyPressClean();
+  }
+  if(dataType == "backspace") {
+    calc.KeyPressBackspace();
+  }
+  if(dataType != null && dataType != "" && dataType != undefined) {
+    if(calc.expression[calc.expression.length-1] != "*" && calc.expression[calc.expression.length-1] != "+" && calc.expression[calc.expression.length-1] != "-" && calc.expression[calc.expression.length-1] != "/" && calc.expression[calc.expression.length-1] != "pow"  && calc.expression[calc.expression.length-1] != "%" && calc.expression[calc.expression.length-1] != "=" && calc.expression[calc.expression.length-1] != "backspace") {
+      calc.TransitionalResult();
+    }
+  }
 });
